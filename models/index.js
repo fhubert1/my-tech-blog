01f675473ce3559e5e6e60e0d5b75529b48bf353
fhubert1 <f.hubert.712@gmail.com>
2024-05-21 11:31:48 -0400

    Add database objects and models

---
+++ models/index.js
@@ -0,0 +1,27 @@
+const User = require('./User');
+const Post = require('./Post');
+//const Comment = require('./Comment');
+
+// post belongs to a user
+Post.belongsTo(User, {
+    foreignKey: 'userId',
+    onDelete: 'CASCADE',
+});
+
+// post can have many comments
+Post.hasMany(Comment, {
+    foreignKey: 'postId',
+    onDelete: 'CASCADE',
+});
+
+// comment can belong to a user
+Comment.belongsTo(User, {
+    foreignKey: 'userId',
+    onDelete: 'CASCADE',
+});
+
+module.exports = {
+    User,
+    Post,
+    Comment,
+};
